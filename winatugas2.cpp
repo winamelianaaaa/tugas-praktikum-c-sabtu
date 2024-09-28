ed97d48e0e63d3e8d1c38f6e2885ca6b78b293a3
winamelianaaaa <winameliana4@gmail.com>
2024-09-28 23:31:58 +0700

    Add files via upload

---
+++ winatugas2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+	int alas = 8;
+	int tinggi = 5;
+	int hasil = alas * tinggi / 2;
+	printf("Luas segitiga dengan ukuran alas 8 cm dan tinggi 5 cm adalah %d cm", hasil);
+	return 0;
+}
