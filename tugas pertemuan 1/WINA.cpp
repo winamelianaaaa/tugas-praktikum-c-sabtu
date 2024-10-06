125dbd6d51ba63946246c9b2cd5159b428d1b6cc
winamelianaaaa <winameliana4@gmail.com>
2024-10-06 22:21:05 +0700

    Add files via upload

---
+++ tugas pertemuan 1/WINA.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+	printf (" NAMA : WINA MELIANA ");
+	printf (" NIM : 2441049 ");
+	printf (" PRODI : TEKNIK INFORMATIKA ");
+	return 0;
+}
+\
---
+++ tugas pertemuan 1/WINA2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+	int panjang, lebar, luaspersegipanjang;
+	
+	panjang=10;
+	lebar=7;
+	
+	luaspersegipanjang=panjang*lebar;
+	printf ("%d", luaspersegipanjang);
+	
+	return 0;
+}
