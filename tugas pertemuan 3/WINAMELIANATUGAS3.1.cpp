6f17099fdea3928191a28351b706aedd9aff9b51
winamelianaaaa <winameliana4@gmail.com>
2024-10-11 18:52:43 +0700

    Update WINAMELIANATUGAS3.1.cpp

--- tugas pertemuan 3/WINAMELIANATUGAS3.1.cpp
+++ tugas pertemuan 3/WINAMELIANATUGAS3.1.cpp
@@ -3,17 +3,14 @@
 int main() {
     float celsius, fahrenheit, reamur;
 
-    // Input suhu dalam Celcius
     printf("Masukkan suhu dalam Celcius: ");
     scanf("%f", &celsius);
 
-    // Menghitung konversi suhu
     fahrenheit = (celsius * 9 / 5) + 32;
     reamur = celsius * 4 / 5;
 
-    // Menampilkan hasil
-    printf("Suhu dalam Fahrenheit: %.2f °F\n", fahrenheit);
-    printf("Suhu dalam Reamur: %.2f °R\n", reamur);
+    printf("Suhu dalam Fahrenheit: %.2f Â°F\n", fahrenheit);
+    printf("Suhu dalam Reamur: %.2f Â°R\n", reamur);
 
     return 0;
 }
